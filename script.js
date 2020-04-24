d740da34b48653d75590654f19ec35947f496c33
jose Lopez <vinnielo01@gmail.com>
2020-04-24 09:57:50 -0700

    fixed fucntions and cleaned up code

--- script.js
+++ script.js
@@ -1,91 +1,72 @@
 // Assignment Code
-function writePassword(){
+function writePassword() {
+  //  prompts that log into letter array
+  var passwordLength = prompt("How many characters? (min 8, max 128)");
 
-var generateBtn = document.querySelector("#generate");
+  // values
 
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  var number = "0123456789".split("");
+  var symbol = "*;<>()[]{}#$?!^|".split("");
 
-var passwordLength = prompt("How many characters? (min 8, max 128)");
-var lowerCase1 = confirm("Do you need lowercase letters?");
-var upperCase1 = confirm("Do you need UPPERCASE letters?");
-var numbers = confirm("Do you need numbers?");
+  var password = "";
 
-var specialChar = confirm("Need special characters? (!$%^&)");
-var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split("");
-var	upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
-var	number = '0123456789'.split("");    
-var	symbol = '*;<>()[]{}#$?!^|'.split("");
+  // letter array stores info
+  var totalChar = [];
 
-var password = "";
-var letterArr = [];
+  // console.log(lowerCase)
 
-console.log(lowerCase)
-    
-// console.log(passwordLength)
-    if (passwordLength >= 8 && passwordLength <= 128)  {       
-            
-            // console.log(password)
-       }  
-       else{
-           
-       } 
-      
+  // if statements for prompts and confirms
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var lowerCase1 = confirm("Do you need lowercase letters?");
+    var upperCase1 = confirm("Do you need UPPERCASE letters?");
+    var numbers = confirm("Do you need numbers?");
+    var specialChar = confirm("Need special characters? (!$%^&)");
     if (lowerCase1 === true) {
-            
-            
-            letterArr = letterArr.concat(lowerCase)
-        }
+      totalChar = totalChar.concat(lowerCase);
+    }
 
     if (upperCase1 === true) {
-        // Logic goes here
-        
-        letterArr = letterArr.concat(upperCase)
+      // Logic goes here
+
+      totalChar = totalChar.concat(upperCase);
     }
 
     if (numbers === true) {
-        // Logic goes here
-        
-        letterArr = letterArr.concat(number)
+      // Logic goes here
+
+      totalChar = totalChar.concat(number);
     }
 
     if (specialChar === true) {
-        // Logic goes here
-        letterArr = letterArr.concat(symbol)
-        
-    }
-    else {
-        
-       
-    }
-console.log(letterArr)
-
-
-
-for(var i=0; i < passwordLength; i++) {
-    var random = Math.floor(Math.random() * letterArr.length);
-    // var lRand = Math.floor(Math.random() * lowerCase.length);
-    // var uRand = Math.floor(Math.random() * upperCase.length);
-    // var num = Math.floor(Math.random() * number.length);
-    // var specC = Math.floor(Math.random() * symbol.length);
-    password = password + letterArr[random]
-
-    console.log(password);       
-        
+      // Logic goes here
+      totalChar = totalChar.concat(symbol);
+    } else {
     }
 
-    document.querySelector("#password").value = password
+    console.log(totalChar);
+
+    // loop for random characters
+
+    for (var i = 0; i < passwordLength; i++) {
+      var random = Math.floor(Math.random() * totalChar.length);
+      // var lRand = Math.floor(Math.random() * lowerCase.length);
+      // var uRand = Math.floor(Math.random() * upperCase.length);
+      // var num = Math.floor(Math.random() * number.length);
+      // var specC = Math.floor(Math.random() * symbol.length);
+      password = password + totalChar[random];
+
+      console.log(password);
+    }
+
+    document.querySelector("#password").value = password;
+  } else {
+    location.reload();
+  }
 }
 
 // // // Add event listener to generate button
 var generateBtn = document.getElementById("generate");
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-   
-  
-
-
-
-
-
